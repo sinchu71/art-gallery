24d82a17c661e069f7b9d45ddc7dd1f8733414a2
sinchana <106988636+sinchu71@users.noreply.github.com>
2023-01-09 15:34:05 +0530

    Add files via upload

---
+++ adminwelcome.php
@@ -0,0 +1,29 @@
+<?php include("header/header.php"); ?>
+<?php
+   if($_SESSION["adminid"] == ""  ||  $_SESSION["adminid"]==NULL)
+   {
+   header('Location:../index.php?page=admin_login');
+   }
+   else{
+   	$a=$_SESSION["adminid"];
+   	?>
+<!-- Banner -->
+<section class="banner full">
+   <article>
+      <img src="../img/admin/bg-4.jpg" alt="" />
+      <div class="inner">
+         <header>
+            <h2>
+               ** Admin Panel ** 
+            </h2>
+            <p><?php echo "<div class='text-center wel-div'>Welcome "." " .$_SESSION["admin"]. "</div>"; ?> </p>
+         </header>
+      </div>
+   </article>
+</section>
+<!-- Visit : www.Amaterasu.com -->
+<?php
+   }
+   mysqli_close($con);
+   ?>
+<?php include("../footer/footer.php"); ?>
---
+++ footer.php
@@ -0,0 +1,24 @@
+<!-- Footer -->
+<footer id="footer">
+   <div class="container">
+      <ul class="icons">
+         <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+         <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+         <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+         <li><a href="#" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+      </ul>
+   </div>
+   <div class="copyright">
+      <div class="footer text-center">Design & Develop By Mayuri K | Visit : www.mayurik.com</div>
+      &copy; MayuriK Artwork Online Studio.All rights reserved.
+   </div>
+</footer>
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.scrollex.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/util.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
